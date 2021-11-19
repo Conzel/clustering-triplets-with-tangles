eb22010ced5726c9832235fa33b94b6d7638ef64
Alex <38732545+Conzel@users.noreply.github.com>
2021-11-20 00:53:45 +0100

    CHANGE: Made profiling script require an output file on command line

--- profiling.py
+++ profiling.py
@@ -2,13 +2,16 @@
 import cProfile
 import pstats
 import yaml
+import sys
 from data_generation import run_experiment, Configuration
+
 
 def run_profiling_experiment():
     with open("experiments/07-performance-improvements.yaml") as f:
         conf = Configuration.from_yaml(yaml.safe_load(f))
 
     run_experiment(conf)
+
 
 def run_cprofile():
     profiler = cProfile.Profile()
@@ -19,6 +22,10 @@ def run_cprofile():
     stats.sort_stats("cumtime")
     return stats
 
+
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("First argument has to be the name of a YAML configuration. Exiting.")
+        exit(1)
     stats = run_cprofile()
-    stats.dump_stats("data_generation.prof")
+    stats.dump_stats(sys.argv[1])
