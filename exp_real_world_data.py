bd31598c57a24a2a055f84dc87fc750b193ce23d
Conzel <almico@Alexs-MacBook-Air.local>
2021-12-17 13:06:48 +0100

    ADD: Standard scaling
    Else a single feature could be a massive detriment in determining
    whether two points are far away from each other.

--- exp_real_world_data.py
+++ exp_real_world_data.py
@@ -3,14 +3,17 @@
 Script for testing tangles algorithm performance on real world datasets.
 """
 
+from matplotlib.pyplot import stackplot
 import sklearn.datasets
 from experiment_runner import tangles_hard_predict
 from questionnaire import generate_questionnaire
 from sklearn.metrics import normalized_mutual_info_score
+from sklearn.preprocessing import StandardScaler
 
 wine = sklearn.datasets.load_wine()
+standardized_data = StandardScaler().fit_transform(wine.data)
 
-questionnaire = generate_questionnaire(wine.data).values
-ys_tangles = tangles_hard_predict(questionnaire, agreement=20)
+questionnaire = generate_questionnaire(standardized_data).values
+ys_tangles = tangles_hard_predict(questionnaire, agreement=7)
 
 print(normalized_mutual_info_score(wine.target, ys_tangles))
