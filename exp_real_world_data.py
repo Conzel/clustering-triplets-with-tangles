7fd60df2411b9d9590442c5fa5f6f060063ea374
Conzel <almico@Alexs-MacBook-Air.local>
2021-12-21 16:23:07 +0100

    CHANGE: Experiment now uses newly written estimators and baselines

--- exp_real_world_data.py
+++ exp_real_world_data.py
@@ -2,42 +2,29 @@
 """
 Script for testing tangles algorithm performance on real world datasets.
 """
-
-from matplotlib.pyplot import stackplot
 import sklearn.datasets
-from experiment_runner import tangles_hard_predict
+from baselines import soe_knn_baseline
+from estimators import OrdinalTangles
+from plotting import AltairPlotter
 from questionnaire import generate_questionnaire
-from sklearn.metrics import normalized_mutual_info_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.manifold import TSNE
-import altair as alt
-import pandas as pd
 
+# Setting up data and transformers
 wine = sklearn.datasets.load_wine()
 standardized_data = StandardScaler().fit_transform(wine.data)
+tangles = OrdinalTangles(agreement=7, verbose=False)
+baseline = soe_knn_baseline(clusters=3)
 
-questionnaire = generate_questionnaire(standardized_data).values
-ys_tangles = tangles_hard_predict(questionnaire, agreement=7)
-
-print(normalized_mutual_info_score(wine.target, ys_tangles))
+questionnaire = generate_questionnaire(standardized_data)
+ys_baseline = baseline.fit_predict(*questionnaire.to_bool_array())
+ys_tangles = tangles.fit_predict(questionnaire.values)
 
 tsne = TSNE(n_components=2)
-tsne_data = tsne.fit_transform(standardized_data)
+embedding = tsne.fit_transform(standardized_data)
 
-# wine_data = pd.DataFrame(wine.data, columns=wine.feature_names)
-tsne_data = pd.DataFrame(tsne_data, columns=["x", "y"])
-tsne_data["label"] = wine.target
-tsne_data["prediction_tangles"] = ys_tangles
-tsne_data["correct"] = tsne_data["label"] == tsne_data["prediction_tangles"]
-chart_true = alt.Chart(tsne_data).mark_point().encode(
-    x="x",
-    y="y",
-    color="label:N"
-).properties(
-    title="True labels"
-)
-chart_pred = chart_true.encode(
-    color="prediction_tangles:N").properties(title="Predicted labels")
+p = AltairPlotter()
 
-chart_combined = (chart_true | chart_pred).properties(title=f"Clustering comparison, NMI = {normalized_mutual_info_score(wine.target, ys_tangles):.2f}")
-chart_combined.show()
+c = p.comparison(embedding, wine.target, ys_tangles) & p.comparison(
+    embedding, wine.target, ys_baseline)
+c.show()
