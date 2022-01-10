7e79118239bd28104526c1e64e2ae43e39f04cb8
Conzel <almico@Alexs-MacBook-Air.local>
2022-01-11 00:11:26 +0100

    CHANGE: Now loops over agreement instead of noise
    This allows us to also plot a baseline

--- exp_noise_agreement.py
+++ exp_noise_agreement.py
@@ -18,6 +18,8 @@ if __name__ == "__main__":
     base_config = Configuration.from_yaml(
         yaml.load(open("experiments/10-noise-agreement.yaml")))
     base_config.redraw_means = True
+    baseline = "soe-knn"
+
     if len(sys.argv) > 1 and (sys.argv[1] == "-p" or sys.argv[1] == "--parallelize"):
         workers = None
     else:
@@ -30,19 +32,35 @@ if __name__ == "__main__":
     base_config.n_runs = 5
 
     variations = []
-    for n in noise:
-        print(n)
-        base_config.noise = n
-        variation = parameter_variation(agreements, "agreement", "agreement",
+    for a in agreements:
+        if a == agreements[-1]:
+            base_config.baseline = baseline
+        print(a)
+        base_config.agreement = a
+        variation = parameter_variation(noise, "noise", "noise",
                                         base_config, workers=None)
         df = variation.to_df()
-        df["noise"] = n
+        df["agreement"] = a
         variations.append(df)
 
+    baseline_df = df.copy()
+    baseline_df["nmi_means_baseline"] = variation.nmi_means_baseline
+    # for the legend
+    baseline_df["agreement"] = baseline
+
     result = pd.concat(variations)
-    alt.Chart(result).mark_line(
+    variation_result = alt.Chart(result).mark_line(
         point={
             "filled": False,
             "fill": "white"
         }
-    ).encode(x="noise", y="nmi", color="agreement:N").show()
+    ).encode(x="noise", y="nmi", color="agreement:N")
+
+    baseline_result = alt.Chart(baseline_df).mark_line(
+        point={
+            "filled": False,
+            "fill": "white"
+        }
+    ).encode(x="noise", y="nmi_means_baseline", color="agreement:N")
+
+    (baseline_result + variation_result).show()
