0111598dc3520ce28d5c0ccb1aa9942a07cadf6c
Conzel <almico@Alexs-MacBook-Air.local>
2022-01-25 00:15:25 +0100

    CHANGE: testcases now also use updated questionnaire generation

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -2,7 +2,7 @@ import unittest
 from sklearn.datasets import load_iris
 from data_generation import generate_gmm_data_fixed_means
 from estimators import OrdinalTangles
-from experiment_runner import generate_questionnaire
+from experiment_runner import Questionnaire
 import numpy as np
 
 
@@ -13,7 +13,7 @@ class OrdinalTanglesTestCase(unittest.TestCase):
     def test_predict_subset_equal(self):
         iris = load_iris()
         tangles = OrdinalTangles(agreement=5, verbose=False)
-        q = generate_questionnaire(iris.data)
+        q = Questionnaire.from_euclidean(iris.data)
 
         tangles.fit(q.values)
         ys1 = tangles.predict(q.values[:10])
@@ -24,7 +24,7 @@ class OrdinalTanglesTestCase(unittest.TestCase):
         synthetic_data = generate_gmm_data_fixed_means(
             n=15, means=np.array(np.array([[0, -10], [-9, 7], [9, 5], [-7, -9], [-10, 0]])), std=0.5, seed=1)
         tangles = OrdinalTangles(agreement=5, verbose=False)
-        q = generate_questionnaire(synthetic_data.xs)
+        q = Questionnaire.from_euclidean(synthetic_data.xs)
         tangles.fit(q.values)
         self.assertAlmostEqual(tangles.score(q.values, synthetic_data.ys), 1.0)
 
