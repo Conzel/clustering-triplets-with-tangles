dc669b3520af5e9a93f31bdb9422459c26c72c71
Conzel <almico@Alexs-MacBook-Air.local>
2022-02-17 23:22:17 +0100

    CHANGE: Made the wine dataset script a bit more expressive
    Fixed titles, embedding dimension and improved runtime by sampling less
    triplets.

--- experiments/10_sklearn_data.py
+++ experiments/10_sklearn_data.py
@@ -5,6 +5,7 @@ Script for testing tangles algorithm performance on real world datasets.
 import sklearn.datasets
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import normalized_mutual_info_score
 
 from baselines import soe_kmeans_baseline
 from estimators import OrdinalTangles
@@ -14,18 +15,22 @@ from questionnaire import Questionnaire
 # Setting up data and transformers
 wine = sklearn.datasets.load_wine()
 standardized_data = StandardScaler().fit_transform(wine.data)
-tangles = OrdinalTangles(agreement=7, verbose=False)
-baseline = soe_kmeans_baseline(clusters=3)
 
-questionnaire = Questionnaire.from_euclidean(standardized_data)
-ys_baseline = baseline.fit_predict(*questionnaire.to_bool_array())
+questionnaire = Questionnaire.from_euclidean(standardized_data, density=0.1)
+
+tangles = OrdinalTangles(agreement=12, verbose=False)
 ys_tangles = tangles.fit_predict(questionnaire.values)
+nmi_tangles = normalized_mutual_info_score(ys_tangles, wine.target)
+
+baseline = soe_kmeans_baseline(clusters=3, data_dimension=2)
+ys_baseline = baseline.fit_predict(*questionnaire.to_bool_array())
+nmi_baseline = normalized_mutual_info_score(ys_baseline, wine.target)
 
 tsne = TSNE(n_components=2)
 embedding = tsne.fit_transform(standardized_data)
 
 p = AltairPlotter()
 
-c = p.comparison(embedding, wine.target, ys_tangles) & p.comparison(
-    embedding, wine.target, ys_baseline)
+c = p.comparison(embedding, wine.target, ys_tangles).properties(title=f"Wine dataset: Tangles (NMI = {nmi_tangles:.2f})") & p.comparison(
+    embedding, wine.target, ys_baseline).properties(title=f"Wine dataset: SOE-kMeans (NMI = {nmi_baseline:.2f})")
 c.show()
