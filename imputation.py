93306b777483d68f167812d8d375254711aae5e3
Conzel <almico@Alexs-MacBook-Air.local>
2022-03-04 11:18:52 +0100

    ADD: Solveig iterative fill

--- imputation.py
+++ imputation.py
@@ -3,10 +3,12 @@ This file contains various methods of imputing numpy arrays.
 
 Mainly used to fill missing values in the questionnaires.
 """
+from math import dist
 import re
 from typing import Callable
 import numpy as np
 from sklearn.impute import KNNImputer
+from sklearn.metrics import pairwise_distances
 
 MISSING_VALUE = -1
 
@@ -77,16 +79,60 @@ class ImputationMethod():
         return imputed_data.round()
 
     @staticmethod
+    def _impute_iterative_fill(data: np.ndarray, i: int):
+        """
+        Does an iterative fill according to the following pseudocode:
+
+        for i in it:
+          - find the pair a,b that have the highest overlap (in terms of questions answered)
+          - if a and b have more than 0.5 agreement, fill gaps with same values, else opposite values
+          - remove a,b from the possible pair 
+        """
+        imputed_data = data.copy()
+        pair_finding_matrix = imputed_data.copy()
+        pair_finding_matrix[pair_finding_matrix == 0] = 1
+        pair_finding_matrix[pair_finding_matrix == MISSING_VALUE] = 0
+        for i in range(i):
+            distances = pairwise_distances(
+                pair_finding_matrix, metric="manhattan")
+            distances[distances == 0] = np.inf
+            a, b = np.unravel_index(distances.argmin(), distances.shape)
+            a_row = imputed_data[a, :]
+            b_row = imputed_data[b, :]
+            a_answered = a_row != MISSING_VALUE
+            b_answered = b_row != MISSING_VALUE
+            both_answered = np.logical_and(a_answered, b_answered)
+            a_not_b_answered = np.logical_and(
+                a_answered, np.logical_not(b_answered))
+            b_not_a_answered = np.logical_and(
+                b_answered, np.logical_not(a_answered))
+            if (a_row[both_answered] == b_row[both_answered]).sum() > 0.5:
+                a_row[b_not_a_answered] = b_row[b_not_a_answered]
+                b_row[a_not_b_answered] = a_row[a_not_b_answered]
+            else:
+                a_row[b_not_a_answered] = np.logical_not(
+                    b_row[b_not_a_answered])
+                b_row[a_not_b_answered] = np.logical_not(
+                    a_row[a_not_b_answered])
+            pair_finding_matrix[a, a_row != MISSING_VALUE] = 1
+            pair_finding_matrix[b, b_row != MISSING_VALUE] = 1
+        return imputed_data
+
+    @staticmethod
     def _parse_imputation(imputation_method_name: str) -> Callable:
         """
         Parses the imputation method from a string.
         """
         knn_regex = re.search(r"(\d+)-NN", imputation_method_name)
+        iterative_fill_regex = re.search(r"(\d+)-fill", imputation_method_name)
         if imputation_method_name.lower() == "random":
             return ImputationMethod._impute_random
         elif knn_regex is not None:
             k = knn_regex.group(1)
             return lambda x: ImputationMethod._impute_knn(x, int(k))
+        elif iterative_fill_regex is not None:
+            i = iterative_fill_regex.group(1)
+            return lambda x: ImputationMethod._impute_iterative_fill(x, int(i))
         elif imputation_method_name.lower() == "mean":
             return ImputationMethod._impute_mean
         else:
