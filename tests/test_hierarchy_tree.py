1e9d35f6c347cdcc8e0642df8fd183d6c4b79bac
Conzel <38732545+Conzel@users.noreply.github.com>
2022-05-24 13:37:08 +0200

    ADD: Some tests for hierarchies

--- tests/test_hierarchy_tree.py
+++ tests/test_hierarchy_tree.py
@@ -48,15 +48,18 @@ def test_raises_non_integers():
 
 
 def test_unbalanced_hierarchy():
-    hier1 = [[[0, 1], [2, 3]], 4, 5, 6]
-    tree = BinaryHierarchyTree(hier1)
-    none_node = tree.root.children[1].children[1]  # type: ignore
-    value_node = tree.root.children[1].children[0]  # type: ignore
-    assert none_node.children == None
-    assert none_node.value == None
-    assert value_node.value == [4, 5, 6]
-    assert tree.clusters_at_level(2) == [[0, 1], [2, 3], [4, 5, 6], []]
-    assert tree.clusters_at_level(1) == [[0, 1, 2, 3], [4, 5, 6]]
+    def test_helper(tree):
+        none_node = tree.root.children[1].children[1]  # type: ignore
+        value_node = tree.root.children[1].children[0]  # type: ignore
+        assert none_node.children == None
+        assert none_node.value == None
+        assert value_node.value == [4, 5, 6]
+        assert tree.clusters_at_level(2) == [[0, 1], [2, 3], [4, 5, 6], []]
+        assert tree.clusters_at_level(1) == [[0, 1, 2, 3], [4, 5, 6]]
+    tree1 = BinaryHierarchyTree([[[0, 1], [2, 3]], 4, 5, 6])
+    tree2 = BinaryHierarchyTree([[[0, 1], [2, 3]], [4, 5, 6]])
+    test_helper(tree1)
+    test_helper(tree2)
 
 
 def test_aari():
